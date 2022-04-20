3bacb6fce3b2d9d63985264857f7e92557b9e1fb
James <54754876+jamesstats@users.noreply.github.com>
2022-04-20 01:29:26 -0400

    Update strikeouts.R

--- strikeouts.R
+++ strikeouts.R
@@ -7,5 +7,5 @@ SO <-"https://www.mlb.com/stats/strikeouts"
 SO_html <-read_html(SO)
 SO_tables <-SO_html%>%html_table(fill=TRUE) 
 ks<-SO_tables[[1]] 
-
+colnames(ks)<-c('Player','Team','G','AB','R','H','2B','3B','HR','RBI','BB','SO','SB','CS','AVG','OBP','SLG','OPS') 
 write_csv(ks,paste0('data/',Sys.Date(),'_StrikeOuts','.csv'))    
