bf7db12676b2758b5296e35252dd20578b6df89a
James <54754876+jamesstats@users.noreply.github.com>
2022-04-20 01:11:51 -0400

    Create strikeouts.R

---
+++ strikeouts.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(rvest)
+library(janitor) 
+ 
+
+SO <-"https://www.mlb.com/stats/strikeouts"
+SO_html <-read_html(SO)
+SO_tables <-SO_html%>%html_table(fill=TRUE) 
+ks<-SO_tables[[1]] 
+
+write_csv(SO,paste0('data/',Sys.Date(),'_StrikeOuts','.csv'))    
