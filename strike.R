626b726ed3afc723685a9eb21494c6f99a17a27f
James <54754876+jamesstats@users.noreply.github.com>
2022-04-22 13:37:18 -0400

    strike.R

--- strike.R
+++ strike.R
@@ -6,9 +6,6 @@ bbk<-"https://www.baseball-reference.com/leagues/majors/2022-batting-leaders.sht
 bbk_html<-read_html(bbk) 
 bbk_tables <-bbk_html%>%html_table(fill=TRUE)
 strikes<-bbk_tables[[20]] 
-colnames(strikes)<-c('Rank','Player','Strikeouts')
-view(strikes) 
-bbkk<-toJSON(strikes)
-write(bbkk, "srikeouts.json")    
+colnames(strikes)<-c('Rank','Player','Strikeouts')   
 
-write(bbkk,paste0('data/strikes.json',Sys.Date(),'_Strike','.json'))    
+write(strikes,paste0('data/strikes.csv',Sys.Date(),'_Strike','.csv'))    
