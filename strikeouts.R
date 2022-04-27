374cfa7149d2c2f41d1d80a7a06f12d433d97b75
James <54754876+jamesstats@users.noreply.github.com>
2022-04-27 09:00:11 -0400

    Update strikeouts.R

--- strikeouts.R
+++ strikeouts.R
@@ -1,11 +1,10 @@
-library(dplyr)
+library(tidyverse)
 library(rvest)
 library(janitor) 
- 
-
-SO <-"https://www.mlb.com/stats/strikeouts"
+SO <-"https://www.espn.com/mlb/stats/player/_/table/batting/sort/strikeouts/dir/desc"
 SO_html <-read_html(SO)
-SO_tables <-SO_html%>%html_table(fill=TRUE) 
-ks<-SO_tables[[1]] 
-colnames(ks)<-c('Player','Team','G','AB','R','H','2B','3B','HR','RBI','BB','SO','SB','CS','AVG','OBP','SLG','OPS') 
-write_csv(ks,paste0('data/',Sys.Date(),'_StrikeOuts','.csv'))    
+SO_tables <-SO_html%>%html_table(fill=TRUE)
+ks_1<-SO_tables[[1]]
+ks<-SO_tables[[2]]
+mlbtbl<-data.frame(ks_1,ks) 
+write_csv(mlbtbl,paste0('data/',Sys.Date(),'_StrikeOuts','.csv'))    
