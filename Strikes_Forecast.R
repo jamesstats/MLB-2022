a934688a485e9e2fa36a76ac72787e938004c9e1
James <54754876+jamesstats@users.noreply.github.com>
2022-05-10 17:11:17 -0400

    Update Strikes_Forecast.R

--- Strikes_Forecast.R
+++ Strikes_Forecast.R
@@ -9,17 +9,20 @@ strikes_1<-strikes_tables[[1]]
 strikes_2<-strikes_tables[[2]]
 mlbtbl<-data.frame(strikes_1,strikes_2)  
 
+getOption('digits')
+options('digits'= 4) 
+
 leaders <-mlbtbl%>%
   select(Name,POS,GP,K)%>%
-  mutate(
-    KAVG = GP/K,
-    GP80 = KAVG*80,
-    GP100 = KAVG*100,
-    GP120 = KAVG*120,
-    GP130 = KAVG*130,
-    GP140 = KAVG*140,
-    GP150 = KAVG*150,
-    GP160 = KAVG*160,
-    GP164 = KAVG*164) 
+   mutate(
+    KPGP = GP/K,
+    GP82 = KPGP*82,
+    GP100 = KPGP*100,
+    GP120 = KPGP*120,
+    GP130 = KPGP*130,
+    GP140 = KPGP*140,
+    GP150 = KPGP*150,
+    GP160 = KPGP*160,
+    GP162 = KPGP*162) 
     
 write_csv(leaders, 'leaders.csv')  
